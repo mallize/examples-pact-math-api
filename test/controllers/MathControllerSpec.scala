863bd02c8888f2df3ef519b21d41890b22942859
Matt Clement <mallize@gmail.com>
2017-11-08 15:20:40 -0600

    Clean up

--- test/controllers/MathControllerSpec.scala
+++ test/controllers/MathControllerSpec.scala
@@ -1,19 +1,10 @@
 package controllers
 
-import org.scalatest.{BeforeAndAfterAll, FunSpec, Matchers, TestData}
-import org.http4s.server.Server
 import com.itv.scalapact.ScalaPactVerify._
-import org.scalatestplus.play.{OneServerPerTest, PlaySpec}
+import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneServerPerTest
-import play.api.Application
-import play.api.inject.guice.GuiceApplicationBuilder
-
 
 class MathControllerSpec extends PlaySpec with GuiceOneServerPerTest {
-
-//  override def newAppForTest(testData: TestData): Application = {
-//    new GuiceApplicationBuilder()
-//  }
 
   "Verify Consumer Contracts" must {
 
@@ -23,9 +14,6 @@ class MathControllerSpec extends PlaySpec with GuiceOneServerPerTest {
           .withPactSource(loadFromLocal("pacts"))
           .noSetupRequired
           .runVerificationAgainst("localhost", 19001)
-
-
-
     }
 
   }
